801c9428261827b028a94367bedd8fab636a6e11
Tim <tim.lucca88@gmail.com>
2019-02-23 15:33:00 -0700

    data

--- Scala/BookStuff/data_structures.scala
+++ Scala/BookStuff/data_structures.scala
@@ -3,16 +3,11 @@ case object Nil extends List[Nothing] // defining the empty list
 case class Cons[+A](head: A, tail: List[A]) extends List[A]
 
 object List {
-    def sum(ints: List[Int]): Int = ints match {
-        case Nil => 0
-        case Cons(x,xs) => x + sum(xs)
-    }
+    def sum(ints: List[Int]): Int =
+        foldRight(ints, 0)((x,y) => x + y)
 
-    def product(ds: List[Double]): Double = ds match {
-        case Nil => 1.0
-        case Cons(0.0, _) => 0.0
-        case Cons(x,xs) => x * product(xs)
-    }
+    def product(ds: List[Double]): Double =
+        foldRight(ds, 1.0)(_ * _)
 
     def apply[A](as: A*): List[A] = 
         if (as.isEmpty) Nil
@@ -29,5 +24,39 @@ object List {
     }
 
     def drop[A](x: List[A], n: Int): List[A] = 
+        if (n == 0) x
+        else x match {
+            case Nil => Nil
+            case Cons(_, y) => drop(y, n-1)
+        }
+    
+    def dropWhile[A](x: List[A])(f: A => Boolean): List[A] = x match {
+        case Cons(h, t) if (f(h)) => dropWhile(t, f)
+        case _ => x
+    }
+
+    def append[A](a1: List[A], a2: List[A]): List[A] = a1 match {
+        case Nil => a2 
+        case Cons(h, t) => Cons(h, appends(t, a2))
+    }
+
+    def init[A](a: List[A]): List[A] = a match {
+        case Nil => Nil
+        case Cons(_, Nil) => Nil 
+        case Cons(h, t) => Cons(h, init(t))
+    }
+
+    def foldRight[A,B](a: List[A], base: B)(f: (A, B) => B): B = a match {
+        case Nil => base 
+        case Cons(h, t) => f(h, foldRight(t, base)(f))
+    }
+
+    def length[A](as: List[A]): Int = 
+        foldRight(as, 0)((_, y) => 1 + y)
+
+    def foldLeft[A,B](a: List[A], base: B)(f: (B, A) => B): B = a match {
+        case Nil => base
+        case Cons(h, t) => foldLeft(t, f(h,b)(f)
+    }
 }
 
