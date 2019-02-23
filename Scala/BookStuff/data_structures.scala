b5a546807788e8ad8cca11584ab5d57ab3b0908f
Tim Lucca <tim.lucca88@gmail.com>
2019-02-23 03:59:55 -0700

    moar list

--- Scala/BookStuff/data_structures.scala
+++ Scala/BookStuff/data_structures.scala
@@ -7,4 +7,27 @@ object List {
         case Nil => 0
         case Cons(x,xs) => x + sum(xs)
     }
+
+    def product(ds: List[Double]): Double = ds match {
+        case Nil => 1.0
+        case Cons(0.0, _) => 0.0
+        case Cons(x,xs) => x * product(xs)
+    }
+
+    def apply[A](as: A*): List[A] = 
+        if (as.isEmpty) Nil
+        else Cons(as.head, apply(as.tail: _*))
+
+    def tail[A](x: List[A]): List[A] = x match {
+        case Nil => Nil
+        case Cons(_, y) => y
+    }
+
+    def setHead[A](x: List[A], h: A): List[A] = x match {
+        case Nil => Cons(h, Nil)
+        case Cons(_, x) => Cons(h, x)
+    }
+
+    def drop[A](x: List[A], n: Int): List[A] = 
 }
+
