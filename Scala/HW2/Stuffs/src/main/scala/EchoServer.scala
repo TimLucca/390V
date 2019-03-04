977ca7fd12ec4b1af5b4bee80025563db61e5844
Tim Lucca <tim.lucca88@gmail.com>
2019-03-03 20:40:17 -0700

    echoserver

--- Scala/HW2/Stuffs/src/main/scala/EchoServer.scala
+++ Scala/HW2/Stuffs/src/main/scala/EchoServer.scala
@@ -3,8 +3,40 @@ import java.io._
 import scala.io._
 
 object EchoServer {
+  val fourOhfour = "404.html"
+  val index = "index.html"
+
+  def getFile(req: String): File = {
+    val file = new File(req)
+    req match {
+      case "/" => new File(index)
+      case _ if(file.exists()) => file
+      case _ => new File(fourOhfour)
+    }
+  }
+
+  def parseInput(input: String): Array[String] = {
+    val tokens: Array[String] = input.split(" ")
+    tokens
+  }
+
+  def fileBytes(file: File): Array[Byte] = {
+    val size: Array[Byte] = new Array(file.length().toInt)
+    size
+  }
+
   def read_and_write(in: BufferedReader, out:BufferedWriter): Unit = {
-    out.write(in.readLine())
+    val input = in.readLine()
+    val tokens = parseInput(input)
+    if(tokens(0)=="GET"){
+      val file = getFile(tokens(1))
+      println(file.getPath())
+      val fileIn = new FileInputStream(file)
+      println(fileIn.read(fileBytes(file)))
+      out.write(file.getPath())
+      println("sent")
+    }
+    
     out.flush()
     in.close()
     out.close()
