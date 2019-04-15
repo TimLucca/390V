018845dc30b0a8187d5a6f2e0fbdfd62ed148616
Tim <tim.lucca88@gmail.com>
2019-04-15 16:55:55 -0600

    e^x now tail recursive

--- Elixir/Practice/e.ex
+++ Elixir/Practice/e.ex
@@ -1,10 +1,13 @@
 defmodule E do 
-    defp pow(x, n) when n == 0, do: 1
-    defp pow(x, n) when n > 0, do: x * pow(x, n - 1)
-    defp fac(num) when num > 1, do: num * fac(num - 1)
-    defp fac(num) when num == 1, do: 1
-    defp calc(x, k) when k == 0, do: 1
-    defp calc(x, k) when k > 0, do: pow(x, k) / fac(k) + calc(x, k - 1)
+    def pow(x, n, acc \\ 1)
+    def pow(x, n, acc) when n == 0, do: acc
+    def pow(x, n, acc) when n > 0, do: pow(x, n - 1, acc * x)
+    def fac(num, acc \\ 1)
+    def fac(num, acc) when num > 1, do: fac(num - 1, acc * num)
+    def fac(num, acc) when num <= 1, do: acc
+    def calc(x, k, acc \\ 1)
+    def calc(x, k, acc) when k == 0, do: acc
+    def calc(x, k, acc) when k > 0, do: calc(x, k-1, pow(x, k) / fac(k) + acc)
     def main(args \\ []) do 
         {x, _} = IO.gets("Enter x: ") |> Integer.parse
         {k, _} = IO.gets("Enter accuracy: ") |> Integer.parse 
